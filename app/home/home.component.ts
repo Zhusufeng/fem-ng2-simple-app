0302f0c30d51e594f3c9199d76c65ce169817fca
Lisa Gee <zhusufeng@gmail.com>
2018-02-04 19:33:17 -0800

    Import Config model to HomeComponent, set Config prop to stateServices Config

--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {StateService} from '../common/state.service';
+import {Config} from '../common/config.model';
 
 @Component({
   selector: 'home',
@@ -10,11 +11,13 @@ export class HomeComponent implements OnInit {
   body:  string = 'This is the about home body';
   greeting: string = 'Hello welcome! This is my property';
   message: string;
+  config: Config;
 
   constructor(private _stateService: StateService) { }
 
   ngOnInit() {
     this.message = this._stateService.getMessage();
+    this.config = this._stateService.config;
   }
 
   updateMessage(m: string): void {
